ebb86ab8c3fa1e9a582418580fb78acf50b3058d
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 <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>
2024-12-30 23:17:28 +0500

    finished 15th in string part

--- lesson-2/string/main-15.py
+++ lesson-2/string/main-15.py
@@ -1,5 +1,8 @@
 #Ask the user for a sentence and create an acronym from the first letters of each word.
 x = input("Enter string: ")
-acronym =''.join([word[0].upper() for word in x.se])
+acronym = x.split(" ")
+result = ""
+for x in acronym:
+    result=result+x[0]
+print(result)
 
-"gapimni holam notogri chundila buvimga gap etudim hol"
