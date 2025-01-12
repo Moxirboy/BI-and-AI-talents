b0c517ca6a59e9e47801ecfef0a91949f242aa9e
mox777boy <mox777boy@gmail.com>
2025-01-12 15:03:06 +0000

    added counter for file

---
+++ lesson-6/homework/count.py
@@ -0,0 +1,28 @@
+import re
+import string
+import os
+
+def count(name='employee.txt'):
+    occurs = {}
+    with open('name',mode='r') as f:
+        line = f.readline()
+        if not line:
+            return occurs
+        words = re.split(r' ',line)
+        for word in words:
+            new = re.sub(f"[{re.escape(string.punctuation)}]", "", word)
+            if new not in occurs:
+                occurs[new] = 1
+            else:
+                occurs[new] +=1
+    return occurs
+try:
+    words = count()
+except FileNotFoundError:
+    print("file not found please enter file name to create")
+    name = input()
+    print("enter your text")
+    with open(name+".txt",mode='w') as f:
+        f.write(input())
+    words = count(name)
+print(words)
